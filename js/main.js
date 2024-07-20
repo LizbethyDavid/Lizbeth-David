addf8ee80c0cf034bd6bef6ee702a395b732de2b
LizbethyDavid <noriegalizbeth42@gmail.com>
2024-07-20 11:02:02 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 const $days = document.getElementById('days'),
 $hours = document.getElementById('hours'),
 $minutes = document.getElementById('minutes'),
-$seconds = document.getElementById('seconds')
+$seconds = document.getElementById('seconds');
+const datoNombre = document.querySelector('#datoNombre');
+const datoMensaje = document.querySelector('#datoMensaje');
+const btnEnviar = document.querySelector('#btnEnviar');
 
+//Fecha a futuro
+const countdownDate = new Date('10 19, 2024 13:00:00').getTime();
+
+var mensaje;
+
+function enviar (){
+    mensaje = `https://api.whatsapp.com/send?phone=524651095580&text=${datoNombre.value}%20${datoMensaje.value} `;
+    btnEnviar.href= mensaje;
+}
 //Fecha a futuro
 const countdownDate = new Date('10 19, 2024 13:00:00').getTime();
 
